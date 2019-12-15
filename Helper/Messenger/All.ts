a82004a9ec6f71e9b533ae473385b16b9eab8973
Dennis Heinrich <dennis.heinrich@posteo.de>
2019-12-15 23:54:44 +0100

    Senden der Nachrichten vorab filtern nach aktivierten Diensten.

--- Helper/Messenger/All.ts
+++ Helper/Messenger/All.ts
@@ -1,10 +1,16 @@
 import {IMessage} from "../../Interfaces/Messages/IMessage";
 import {TelegramHelper} from "./Telegram";
 import {DiscordHelper} from "./Discord";
+const Configuration = require("../../Configuration");
 
 export class AllMessageSplitter {
     public static async SplitMessage(NMessage: IMessage) : Promise<void> {
-        await TelegramHelper.CreateTelegramMessage(NMessage);
-        await DiscordHelper.CreateDiscordMessage(NMessage);
+        if(Configuration.Telegram.Enabled) {
+            await TelegramHelper.CreateTelegramMessage(NMessage);
+        }
+
+        if(Configuration.Discord.Enabled) {
+            await DiscordHelper.CreateDiscordMessage(NMessage);
+        }
     }
 }
