959039a8aa23c8597a64327b903112dff98c00c6
Dennis Heinrich <dennis.heinrich@posteo.de>
2019-12-16 05:05:06 +0100

    Merge branch 'master' of C:\Users\denni\Desktop\Entwicklung\uelzen_bot with conflicts.

--- Configuration.ts
+++ Configuration.ts
@@ -4,8 +4,8 @@ module.exports = {
     },
     Telegram: {
         Enabled: true,
-        ChatId: -100000000,
-        BotToken: "XXXXXXXXXX",
+        ChatId: -1001266018619,
+        BotToken: "1012885395:AAGb798lkuGY5hfPXkH0LMxZDa-DxGzNryE",
     },
     Discord: {
         Enabled: false,
@@ -13,20 +13,24 @@ module.exports = {
     },
     Services: {
         AZ_Online: {
-            Enabled: true,
+            Enabled: false,
             ServiceFeedUrl: "https://www.az-online.de/uelzen/rssfeed.rdf",
         },
         UelzenTV: {
-            Enabled: true,
+            Enabled: false,
             ServiceFeedUrl: "https://www.uelzen-tv.com/nachrichten-aktuelles-uelzen?format=feed&type=rss",
         },
         Hansestadt_Uelzen: {
-            Enabled: true,
+            Enabled: false,
             ServiceFeedUrl: "https://www.hansestadt-uelzen.de/contentxxl/services/export/getcontent.aspx?mid=341&mdefid=71&eid=1",
         },
         FF_UE_Einsatzberichte: {
-            Enabled: true,
+            Enabled: false,
             ServiceFeedUrl: "https://feuerwehr-uelzen.de/einsaetze/feed/",
+        },
+        Verkehrsmeldungen: {
+            Enabled: true,
+            ServiceFeedUrl: "https://www.verkehrsinfo.de/httpsmobil/index.php?c=staulist&lat=53&lon=10"
         }
     }
 };
---
+++ Services/Verkehrsmeldungen/Verkehrsmeldungen.ts
@@ -0,0 +1,54 @@
+import {IService} from "../../Interfaces/IService";
+import {ServiceDataStore} from "../../Helper/ServiceDataStore";
+import {AllMessageSplitter} from "../../Helper/Messenger/All";
+import {Message} from "../../Interfaces/Messages/Message";
+const Configuration = require("../../Configuration");
+const {JSDOM} = require("jsdom");
+const utf8 = require('utf8');
+
+export class Verkehrsmeldungen implements IService{
+    current_count: number = 0;
+    id: string = "verkehrsmeldungen";
+    name: string = "Verkehrsmeldungen";
+    store: ServiceDataStore = new ServiceDataStore(this.id);
+    _request: any = require("request");
+
+    AddUpdatedMessage() {
+        this.current_count += 1;
+    }
+
+    ClearUpdatedMessages() {
+        this.current_count = 0;
+    }
+
+    GetUpdatedMessages() {
+        return this.current_count;
+    }
+
+    async UpdateServiceTick() {
+        let that = this;
+        this._request(Configuration.Services.Verkehrsmeldungen.ServiceFeedUrl, function (err, res, body) {
+            let dom = JSDOM.fragment(body);
+            let Query = dom.querySelectorAll("div.panel-body ul li .message");
+            for (let i = 0; i < Query.length; i++) {
+                let ReportHeader = Query[i].children[0].innerHTML;
+                let ReportMessage = Query[i].children[2].innerHTML;
+
+                let NMessage = new Message();
+                NMessage.setContentOwner(that.name);
+                NMessage.setTitle(ReportHeader);
+                NMessage.setMessage(ReportMessage);
+                NMessage.setCreationTime(new Date());
+
+                if(!that.store.IsStored(NMessage) && NMessage.getTitle().includes("Uelzen")) {
+                    console.info(" * "+ that.name + ": " + NMessage.getTitle());
+                    that.store.Store(NMessage);
+                    AllMessageSplitter.SplitMessage(NMessage).catch(reason => {
+                        that.
+                        console.log(reason);
+                    })
+                }
+            }
+        });
+    }
+}
